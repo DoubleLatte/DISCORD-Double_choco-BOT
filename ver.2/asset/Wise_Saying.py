44d924368021111c6b14e6b9dae8306b0d97b352
DoubleLatte <85533915+DoubleLatte@users.noreply.github.com>
2022-07-08 13:39:37 +0900

    Create Wise_Saying.py

---
+++ ver.2/asset/Wise_Saying.py
@@ -0,0 +1,11 @@
+#----모듈입력----
+form bs4 import beautifulsoup as soup
+import os
+import usecsv
+import request
+
+#----구동부----
+url = 'input'
+html = ur.urlopen(url)
+soup = bs(html.read(),'html.parser')
+
